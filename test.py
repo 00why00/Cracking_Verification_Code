31ae4824aedeaa037011b7cf51681acb3635d47c
00why00 <894321963@163.com>
2019-12-17 19:08:59 +0800

    init project

--- test.py
+++ test.py
@@ -1,9 +1,8 @@
+# -*- encoding：utf-8 -*-
 """
 @project: Cracking_Verification_Code
 @author: why
 @file: test.py
 @ide: PyCharm
-@time: 2019/7/23 13:32
+@time: 2019/12/17 19:08
 """
-
-# print(tf.version)
